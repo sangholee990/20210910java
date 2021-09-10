d4bf456bb5d97f36c42df9a6be75d5e532ce47b7
sangholee990 <80809068+sangholee990@users.noreply.github.com>
2021-09-10 15:48:36 +0900

    Create no2.java

---
+++ no2.java
@@ -0,0 +1,28 @@
+package javatest1;
+
+
+public class main {
+	
+	
+
+	public static void main(String[] args) {
+		
+		int a = in.nextInt();
+		
+		int b = a%10;
+		int c = a/10;
+		
+		if (b==c) {
+			System.out.println("Yes! 10의자 자리와 1의 자리가 같습니다.");
+		}
+		
+		else {
+			System.out.println("no! 10의자 자리와 1의 자리가 같지 않습니다.");
+		}
+		
+		
+		
+		
+	}
+
+}
